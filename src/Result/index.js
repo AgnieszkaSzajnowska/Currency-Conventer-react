0316c0c1cdff9aff63db06107c6f857be3363483
Agnieszka Szajnowska <agnieszka.badyla@o2.pl>
2025-04-07 14:22:02 +0200

    calculation form added

---
+++ src/Result/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+export const Result = ({result}) => (
+    <p className="result">
+        {result !== undefined && (
+            <>
+            {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
+            {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+            </>
+        )}
+    </p>
+);
